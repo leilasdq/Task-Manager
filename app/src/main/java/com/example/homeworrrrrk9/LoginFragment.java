161115d7468ebddf9e8001df86a4e5296784c535
User <l.sedighi.2015@example.com>
2019-09-15 17:50:07 +0430

    button_style: make a file to set a style to login page. Colors: add colors to use in style and change app colors. LoginFragment: find views in it's xml.

--- app/src/main/java/com/example/homeworrrrrk9/LoginFragment.java
+++ app/src/main/java/com/example/homeworrrrrk9/LoginFragment.java
@@ -8,13 +8,19 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.google.android.material.textfield.TextInputLayout;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class LoginFragment extends Fragment {
-
+    private TextInputLayout mUserInput;
+    private TextInputLayout mPasswordInput;
+    private Button mLoginButton;
+    private Button mSignUpButton;
 
     public LoginFragment() {
         // Required empty public constructor
@@ -34,7 +40,14 @@ public class LoginFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false);
+        View view = inflater.inflate(R.layout.fragment_login, container, false);
+
+        mUserInput = view.findViewById(R.id.text_input_user_name);
+        mPasswordInput = view.findViewById(R.id.text_input_password);
+        mLoginButton = view.findViewById(R.id.login_btn);
+        mSignUpButton = view.findViewById(R.id.create_account_btn);
+
+        return view;
     }
 
 }
