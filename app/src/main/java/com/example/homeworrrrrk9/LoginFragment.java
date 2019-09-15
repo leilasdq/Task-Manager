1414e694eb7f4ba88cbe9641635ac414b0d0e892
User <l.sedighi.2015@example.com>
2019-09-15 18:19:30 +0430

    add some methods to handle TextInputType.

--- app/src/main/java/com/example/homeworrrrrk9/LoginFragment.java
+++ app/src/main/java/com/example/homeworrrrrk9/LoginFragment.java
@@ -3,6 +3,7 @@ package com.example.homeworrrrrk9;
 
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -10,6 +11,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
+import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.textfield.TextInputLayout;
 
 
@@ -35,6 +37,12 @@ public class LoginFragment extends Fragment {
         return fragment;
     }
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -47,7 +55,47 @@ public class LoginFragment extends Fragment {
         mLoginButton = view.findViewById(R.id.login_btn);
         mSignUpButton = view.findViewById(R.id.create_account_btn);
 
+        mLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!userValidate() | !passwordValidate()){
+                    Snackbar.make(getView(), "Fill the realignments..", Snackbar.LENGTH_LONG).show();
+                }
+                else {
+                    Snackbar.make(getView(), "You are logged in", Snackbar.LENGTH_LONG).show();
+                }
+            }
+        });
+
         return view;
     }
+
+    private boolean userValidate() {
+        String userText = mUserInput.getEditText().getText().toString();
+
+        if (userText.isEmpty()) {
+            mUserInput.setError("Field can not be empty");
+            return false;
+        } if (userText.length() > 15){
+            mUserInput.setError("Too long text");
+            return false;
+        } else {
+            mUserInput.setError(null);
+            return true;
+        }
+    }
+
+    private boolean passwordValidate(){
+        String passText = mPasswordInput.getEditText().getText().toString();
+
+        if (passText.isEmpty()) {
+            mPasswordInput.setError("Field can not be empty");
+            return false;
+        } else {
+            mPasswordInput.setError(null);
+            return true;
+        }
+    }
+
 
 }
