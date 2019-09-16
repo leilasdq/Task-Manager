0435633ad9e73c6c84066ffedb4d05fc266942af
User <l.sedighi.2015@example.com>
2019-09-16 17:12:50 +0430

    login will get data from sign up page and if all things all ok it will send a snack msg to say it's all correct.

--- app/src/main/java/com/example/homeworrrrrk9/LoginFragment.java
+++ app/src/main/java/com/example/homeworrrrrk9/LoginFragment.java
@@ -1,11 +1,15 @@
 package com.example.homeworrrrrk9;
 
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,19 +23,32 @@ import com.google.android.material.textfield.TextInputLayout;
  * A simple {@link Fragment} subclass.
  */
 public class LoginFragment extends Fragment {
+    public static final int LOGIN_FRAGMENT_REQUEST_CODE = 1;
+    private final String FRAGMENT_TAG_SIGN_UP_FRAGMENT = "sign up fragment";
+
+    public static final String TAG = "Login fragment";
+
     private TextInputLayout mUserInput;
     private TextInputLayout mPasswordInput;
     private Button mLoginButton;
     private Button mSignUpButton;
+
+    //// USER AND PASSWORD ON THIS PAGE ////
+    private String userText;
+    private String passText;
+
+    //// USER AND PASS COME FROM SIGN UP PAGE ////
+    String getUserName;
+    String getPassword;
 
     public LoginFragment() {
         // Required empty public constructor
     }
 
     public static LoginFragment newInstance() {
-        
+
         Bundle args = new Bundle();
-        
+
         LoginFragment fragment = new LoginFragment();
         fragment.setArguments(args);
         return fragment;
@@ -40,8 +57,6 @@ public class LoginFragment extends Fragment {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-
     }
 
     @Override
@@ -50,33 +65,96 @@ public class LoginFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_login, container, false);
 
+        initViews(view);
+        setUpListeners();
+
+        return view;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode != Activity.RESULT_OK || data == null)
+            return;
+
+        if (requestCode == LOGIN_FRAGMENT_REQUEST_CODE) {
+
+            Log.e(TAG, "onActivityResult: user: " + mUserInput.getEditText().getText().toString() +
+                    "\npass: " + mPasswordInput.getEditText().getText().toString());
+
+            getUserName = data.getStringExtra(SignUpFragment.EXTRA_USER_NAME);
+            getPassword = data.getStringExtra(SignUpFragment.EXTRA_PASS_WORD);
+
+//            //Log.e(TAG, "sendResult: user: " + getUserName + "\npass: " + getPassword );
+//
+//
+//
+//            Log.e(TAG, "onActivityResult: user: " + mUserInput.getEditText().getText().toString() +
+//                    "\npass: " + mPasswordInput.getEditText().getText().toString());
+        }
+    }
+
+    private void initViews(View view) {
         mUserInput = view.findViewById(R.id.text_input_user_name);
         mPasswordInput = view.findViewById(R.id.text_input_password);
         mLoginButton = view.findViewById(R.id.login_btn);
         mSignUpButton = view.findViewById(R.id.create_account_btn);
 
+        mUserInput.getEditText().setText(getUserName);
+        mPasswordInput.getEditText().setText(getPassword);
+    }
+
+    private void setUpListeners() {
+
         mLoginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (!userValidate() | !passwordValidate()){
+                if (!userValidate() | !passwordValidate()) {
                     Snackbar.make(getView(), "Fill the realignments..", Snackbar.LENGTH_LONG).show();
-                }
-                else {
+                } else if (mUserInput.getEditText().getText().toString().equals(getUserName)
+                && mPasswordInput.getEditText().getText().toString().equals(getPassword)){
                     Snackbar.make(getView(), "You are logged in", Snackbar.LENGTH_LONG).show();
+                } else {
+                    Snackbar.make(getView(), "invalid username or password", Snackbar.LENGTH_LONG).show();
                 }
             }
         });
 
-        return view;
+        mSignUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String username = mUserInput.getEditText().getText().toString();
+                String password = mPasswordInput.getEditText().getText().toString();
+
+                //Log.e(TAG, "newInstance: user: " + username +"\npass: " + password );
+
+                FragmentManager fragmentManager = getFragmentManager();
+                SignUpFragment signUpFragment = SignUpFragment.newInstance(username, password);
+                signUpFragment.setTargetFragment(LoginFragment.this, LOGIN_FRAGMENT_REQUEST_CODE);
+
+                //Log.e(TAG, "onClick: TargetFragment: " + signUpFragment.getTargetFragment());
+
+                mUserInput.getEditText().setText("");
+                mPasswordInput.getEditText().setText("");
+
+
+                fragmentManager.beginTransaction()
+                        .addToBackStack(null)
+                        .replace(R.id.container, signUpFragment, FRAGMENT_TAG_SIGN_UP_FRAGMENT)
+                        .commit();
+            }
+        });
     }
 
     private boolean userValidate() {
-        String userText = mUserInput.getEditText().getText().toString();
+        userText = mUserInput.getEditText().getText().toString();
 
         if (userText.isEmpty()) {
             mUserInput.setError("Field can not be empty");
             return false;
-        } if (userText.length() > 15){
+        }
+        if (userText.length() > 15) {
             mUserInput.setError("Too long text");
             return false;
         } else {
@@ -85,8 +163,8 @@ public class LoginFragment extends Fragment {
         }
     }
 
-    private boolean passwordValidate(){
-        String passText = mPasswordInput.getEditText().getText().toString();
+    private boolean passwordValidate() {
+        passText = mPasswordInput.getEditText().getText().toString();
 
         if (passText.isEmpty()) {
             mPasswordInput.setError("Field can not be empty");
@@ -96,6 +174,4 @@ public class LoginFragment extends Fragment {
             return true;
         }
     }
-
-
 }
--- app/src/main/java/com/example/homeworrrrrk9/SignUpFragment.java
+++ app/src/main/java/com/example/homeworrrrrk9/SignUpFragment.java
@@ -1,40 +1,123 @@
 package com.example.homeworrrrrk9;
 
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.google.android.material.textfield.TextInputLayout;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class SignUpFragment extends Fragment {
+    public static final String ARGS_USERNAME_TEXT = "username text";
+    public static final String ARGS_PASSWORD_TEXT = "password text";
+
+    public static final String TAG = "SignUp fragment";
+    public static final String EXTRA_USER_NAME = "user name";
+    public static final String EXTRA_PASS_WORD = "pass word";
+
+    private TextInputLayout mUserInput;
+    private TextInputLayout mPasswordInput;
+    private Button mSignButton;
+
+    String getUserText;
+    String getPassText;
+
+    String userName;
+    String password;
 
 
     public SignUpFragment() {
         // Required empty public constructor
     }
 
-    public static SignUpFragment newInstance() {
-        
+    public static SignUpFragment newInstance(String user, String pass) {
+
         Bundle args = new Bundle();
-        
+        args.putString(ARGS_USERNAME_TEXT, user);
+        args.putString(ARGS_PASSWORD_TEXT, pass);
+
+//        Log.e(TAG, "newInstance: user: " + user + "\npass: " + pass);
+
         SignUpFragment fragment = new SignUpFragment();
         fragment.setArguments(args);
         return fragment;
     }
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (getArguments() != null) {
+            getUserText = getArguments().getString(ARGS_USERNAME_TEXT);
+            getPassText = getArguments().getString(ARGS_PASSWORD_TEXT);
+        }
+
+//        Log.e(TAG, "newInstance: user: " + getUserText + "\npass: " + getPassText);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sign_up, container, false);
+        View view = inflater.inflate(R.layout.fragment_sign_up, container, false);
+
+        initViews(view);
+
+        mSignButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                userName = mUserInput.getEditText().getText().toString();
+                password = mPasswordInput.getEditText().getText().toString();
+
+                sendResult();
+            }
+        });
+
+
+        return view;
+    }
+
+    private void initViews(View view) {
+        mUserInput = view.findViewById(R.id.text_user_name);
+        mPasswordInput = view.findViewById(R.id.text_password);
+        mSignButton = view.findViewById(R.id.signUp_btn);
+
+        mUserInput.getEditText().setText(getUserText);
+        mPasswordInput.getEditText().setText(getPassText);
+
+    }
+
+    private void sendResult() {
+        userName = mUserInput.getEditText().getText().toString();
+        password = mPasswordInput.getEditText().getText().toString();
+        Log.e(TAG, "sendResult: user: " + userName + "\npass: " + password );
+
+        Intent intent = new Intent();
+        intent.putExtra(EXTRA_USER_NAME, userName);
+        intent.putExtra(EXTRA_PASS_WORD, password);
+
+        Fragment fragment = getTargetFragment();
+//        Log.e(TAG, "sendResult: Target Fragment: " + getTargetFragment() );
+        getFragmentManager().beginTransaction().replace(R.id.container, getTargetFragment()).commit();
+        fragment.onActivityResult(getTargetRequestCode(), Activity.RESULT_OK, intent);
+
+//        getFragmentManager().beginTransaction()
+//                .replace(R.id.container, fragment)
+//                .commit();
+
     }
 
 }
