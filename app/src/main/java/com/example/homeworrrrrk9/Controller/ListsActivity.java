7b8c17a257742f2e365cdc54772211caa7c2c30d
User <l.sedighi.2015@example.com>
2019-09-16 19:57:46 +0430

    set TabLayout with fragments.

--- app/src/main/java/com/example/homeworrrrrk9/Controller/ListsActivity.java
+++ app/src/main/java/com/example/homeworrrrrk9/Controller/ListsActivity.java
@@ -1,14 +1,23 @@
 package com.example.homeworrrrrk9.Controller;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.homeworrrrrk9.R;
+import com.example.homeworrrrrk9.TaskManagerFragmentPagerAdapter;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.tabs.TabLayout;
 
 public class ListsActivity extends AppCompatActivity {
+
+    ViewPager mViewPager;
+    TabLayout mTabLayout;
+    FloatingActionButton addBtn;
 
     public static Intent newIntent(Context context){
         return new Intent(context, ListsActivity.class);
@@ -18,5 +27,17 @@ public class ListsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lists);
+
+        initViews();
+    }
+
+    private void initViews(){
+        mViewPager = findViewById(R.id.task_view_pager);
+        addBtn = findViewById(R.id.floatingActionButton);
+        mTabLayout = findViewById(R.id.task_tab_layout);
+
+        TaskManagerFragmentPagerAdapter adapter = new TaskManagerFragmentPagerAdapter(getSupportFragmentManager());
+        mViewPager.setAdapter(adapter);
+        mTabLayout.setupWithViewPager(mViewPager);
     }
 }
