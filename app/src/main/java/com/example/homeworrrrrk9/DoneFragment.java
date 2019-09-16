07b2f20a50a4cd4d8008ec9dfe1abcefac5ba242
User <l.sedighi.2015@example.com>
2019-09-16 17:26:34 +0430

    add 3 fragments to show list items of each list

---
+++ app/src/main/java/com/example/homeworrrrrk9/DoneFragment.java
@@ -0,0 +1,40 @@
+package com.example.homeworrrrrk9;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class DoneFragment extends Fragment {
+
+    public static DoneFragment newInstance() {
+        
+        Bundle args = new Bundle();
+        
+        DoneFragment fragment = new DoneFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+
+    public DoneFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_done, container, false);
+    }
+
+}
---
+++ app/src/main/java/com/example/homeworrrrrk9/TodoFragment.java
@@ -0,0 +1,40 @@
+package com.example.homeworrrrrk9;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class TodoFragment extends Fragment {
+
+    public static TodoFragment newInstance() {
+        
+        Bundle args = new Bundle();
+        
+        TodoFragment fragment = new TodoFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+
+    public TodoFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_todo, container, false);
+    }
+
+}
